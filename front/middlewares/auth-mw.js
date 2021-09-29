7eba5531d62d5bad9866a0e7e119b5316b9d9d6e
1binibin <namwonbin@gmail.com>
2021-09-29 15:49:01 +0900

    first commit - 기본 구성 설정

---
+++ front/middlewares/auth-mw.js
@@ -0,0 +1,30 @@
+const { pool } = require('../modules/mysql-init')
+const { alert } = require('../modules/util')
+const { findMyBook } = require('../models/book')
+
+const isUser = (req, res, next) => {
+	if(req.user) next()
+	else res.send( alert('로그인 후 이용하세요.') )
+}
+
+const isGuest = (req, res, next) => {
+	if(req.user) res.send( alert('회원은 이용하실수 없습니다.') )
+	else next()
+}
+
+const isMyBook = (name, mode) => {
+	return async (req, res, next) => { 
+		let sql
+		const { idx, _method } = eval(`req.${name}`)
+		const fidx = req.user.idx
+		console.log(idx, fidx, _method)
+		if(mode === 'U' && _method !== 'PUT')  next()
+		else {
+			const { success } = await findMyBook(idx, fidx)
+			if(success) next()
+			else res.send(alert('정상적인 접근이 아닙니다. -.-'))
+		}
+	}
+}
+
+module.exports = { isUser, isGuest, isMyBook }
---
+++ front/middlewares/multer-book-mw.js
@@ -0,0 +1,48 @@
+const multer = require('multer')
+const moment = require('moment')
+const path = require('path')
+const fs = require('fs-extra')
+const { v4: uuid } = require('uuid')
+const { exts } = require('../modules/util')
+const	allowExt = [...exts.imgExt, ...exts.docExt, ...exts.mediaExt, ...exts.zipExt]
+const mega = 1024000
+
+
+const destination = async (req, file, cb) => {
+	try{
+		const folder = path.join(__dirname, '../storages',  moment().format('YYMMDD'))
+		await fs.ensureDir(folder)
+		cb(null, folder)
+	}
+	catch(err) {
+		cb(err)
+	}
+}
+
+const filename = (req, file, cb) => {
+	try{
+		const ext = path.extname(file.originalname).toLowerCase()		// .jpg path.extname: 확장자 이름 가져와~
+		const filename = moment().format('YYMMDD') + '_'+ uuid() + ext
+		cb(null, filename)
+	}
+	catch(err) {
+		cb(err)
+	}
+}
+
+const fileFilter = (req, file, cb) => {	// 어느 파일을 업로드 하거나, 건너뛸지 제어하는함수
+  try{
+		const ext = path.extname(file.originalname).substr(1).toLowerCase()
+			if((file.fieldname === 'cover' ? exts.imgExt : allowExt).includes(ext)) cb(null, true)
+			else cb(new Error(`첨부하신 파일은 업로드가 허용되지 않습니다. -${ext}파일 `))
+	}
+	catch(err) {
+		cb(err)
+	}
+}
+
+const storage = multer.diskStorage({ destination, filename })
+const limits = { fileSize: mega * 5}	// fileSize: multiform형식에서 최대 파일 사이즈(bytes)
+
+module.exports = multer({ storage, limits, fileFilter })
+
