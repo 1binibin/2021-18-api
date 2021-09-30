dd2814762d0aeae589d90e9de7c2c0e3fd536a04
1binibin <namwonbin@gmail.com>
2021-09-30 13:19:09 +0900

    accessToken 발행

--- back/routes/api/api-router.js
+++ back/routes/api/api-router.js
@@ -1,11 +1,28 @@
-const path = require('path')
 const express = require('express')
 const router = express.Router()
-const { error } = require('../../modules/util')
-// const { pool } = require('../../modules/mysql-init')
+const jwt = require('jsonwebtoken')
+const { pool } = require('../../modules/mysql-init')
 
-router.get('/', (req, res, next) => {
-
+router.get('/', async (req, res, next) => {
+	let token = req.headers.authorization
+	let sql
+	try {
+		jwt.verify(token, process.env.JWT_SALT, async (err, decoded) => {
+			if(err) {
+				// res.status(401).json({ err: '토큰이 만료되었습니다.' }) 
+				data = { idx: rs[0].idx, userid: rs[0].userid }
+				token = jwt.sign(data, process.env.JWT_SALT, { expiresIn: '10s' })
+			}
+			else {
+				sql = " SELECT * FROM books ORDER BY idx DESC "
+				const [books] = await pool.execute(sql)
+				res.status(200).json({ success: true, books })
+			}
+		})
+	}
+	catch (err) {
+		res.status(500).json(err)
+	}
 })
 
 
--- front/public/js/front.js
+++ front/public/js/front.js
@@ -1,17 +1,32 @@
 $('.bt-token').click(onGetToken)
-function onGetToken() {
-	let userid = 'wonbin';
-	let apikey = '2dd82e6d-652f-4242-ae65-4b694dae1a25';
-	let apiURL = 'http://127.0.0.1:3100/api/sign';
-	var data = { userid: userid, apikey: apikey }
-	axios.post(apiURL, data).then(onResult).catch(onError);
+$('.bt-data').click(onGetData)
 
-	function onResult(r) {
+var token = null;
+let userid = 'wonbin';
+let apikey = '2dd82e6d-652f-4242-ae65-4b694dae1a25';
+let tokenURL = 'http://127.0.0.1:3100/api/sign';
+let dataURL = 'http://127.0.0.1:3100/api';
+
+function onGetToken() {
+	var data = { userid: userid, apikey: apikey }
+	axios.post(tokenURL, data).then(getToken).catch(onError);
+
+	function getToken(r) {
+		token = r.data.token;
+		console.log(token)
+	}
+}
+
+
+function onGetData() {
+	let headers = { authorization: token }
+	axios.get(dataURL, { headers }).then(getData).catch(onError);
+
+	function getData(r) {
 		console.log(r.data);
 	}
-
+}
 
 	function onError(err) {
 		console.log(err);
 	}
-}
